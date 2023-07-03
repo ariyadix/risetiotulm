49befd67b67ffae2077fd08bd64695d25b76ee3e
ariyadix <86592385+ariyadix@users.noreply.github.com>
2023-07-03 15:42:23 +0800

    Update tambahdataaktual.php

--- tambahdataaktual.php
+++ tambahdataaktual.php
@@ -23,12 +23,13 @@ if (isset($_POST['submit'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <div id="time" class="mx-auto"></div>
     <title>Tambah Data Aktual</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-        <div id="time" class="mx-auto"></div>
+       
         <h2>Tambah Data Aktual</h2>
         <!-- set action to this file -->
         <form action="<?= $_SERVER['PHP_SELF'] ?>" method="post">
